085b6490c0ea8f58a52515f6c9a7071671bf9b24
aditya-1508 <aditya.chandrakar@ssipmt.com>
2025-07-25 23:31:12 +0530

    Add frontend folder

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ mongoose
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB connection error:', err));
 
-const PORT =5000;
+const PORT =process.env.PORT||5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
